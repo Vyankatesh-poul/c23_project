bc31df4c6504677bd00752c050c0ce5d531e4599
Vyankatesh <60972448+Vyankatesh-poul@users.noreply.github.com>
2020-04-01 19:44:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+function draw() {
+  background("skyBlue");  
+  
+  fill(204,204,204);
+  rect(20,80,150,300);
+  
+  fill("black");
+  rect(0,380,1200,30);
+
+  fill(204,204,204);
+  rect(628,80,150,300);
+  
+  fill(204,204,204);
+  rect(170,147,458,232);
+  
+  fill(153,102,0);
+  rect(247,205,309,174)
+
+  fill(0,0,0);
+  rect(20,55,25,25);
+
+  rect(63,55,25,25);
+
+  rect(103,55,25,25);
+
+  rect(143,55,25,25);
+  
+  rect(627,55,25,25);
+
+  rect(667,55,25,25);
+
+  rect(710,55,25,25);
+
+  rect(750,55,25,25);
+
+  
+    
+  
+  
+  
+  drawSprites();
+  text("King Vyankatesh's fort",345,235);
+}
